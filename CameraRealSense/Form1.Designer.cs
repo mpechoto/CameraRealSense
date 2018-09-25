0aa0e0ddc620f4210838bd20a843f4cde286434c
mpechoto <murilomarin@gmail.com>
2018-09-25 19:59:04 -0300

    POC

--- CameraRealSense/Form1.Designer.cs
+++ CameraRealSense/Form1.Designer.cs
@@ -29,12 +29,43 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(97, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 207);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- CameraRealSense/Form1.cs
+++ CameraRealSense/Form1.cs
@@ -10,11 +10,74 @@ using System.Windows.Forms;
 
 namespace CameraRealSense
 {
+    
+
+
     public partial class Form1 : Form
     {
+        //public PXCMSession session = PXCMSession.CreateInstance();
+
+        public PXCMSenseManager sm = PXCMSenseManager.CreateInstance();
+        
+        //PXCMSession.ImplVersion v = sm.session.version;
+
+        
+
+
+
         public Form1()
         {
             InitializeComponent();
+            //PXCMSession.ImplVersion v = sm.session.QueryVersion() ;
+            //Console.WriteLine("SDK Version: {0}.{1}\n", v.major, v.minor);
+            //Console.WriteLine("SDK Version: {0}.{1}", session.QueryVersion().major, session.QueryVersion().minor);
+            // Set file playback name
+            sm.captureManager.SetFileName("filename4.rssdk",true);
+
+            sm.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_COLOR, 640, 480, 30);
+            sm.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_DEPTH, 0,0);
+            sm.Init();
+            // Set realtime=true and pause=false
+            //sm.captureManager.SetRealtime(false);
+            //sm.captureManager.SetPause(true);
+
+            for (; ; )
+            {
+                //pxcmStatus sts = sm.AcquireFrame(true);
+                if (sm.AcquireFrame(true).IsError())
+                {
+                    Console.WriteLine("break");
+                    break;
+                }
+                //PXCMCapture.Sample sample = sm.QuerySample();
+                //pictureBox1.Image = sample.color;
+                // process image
+                //sm.ReleaseFrame();
+                // retrieve the samples
+                PXCMCapture.Sample sample = sm.QuerySample();
+                if (sample != null)
+                {
+                    if (sample.color != null)
+                    {
+                        // work on the color sample
+                        Console.Write("color");
+                    }
+                    if (sample.depth != null)
+                    {
+                        // work on the depth sample
+                        Console.Write("depth");
+                    }
+                }
+                // go fetching the next samples
+                sm.ReleaseFrame();
+            
+            }
+                sm.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
